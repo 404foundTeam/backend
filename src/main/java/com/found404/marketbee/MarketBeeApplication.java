9b4ce30106a8b067850e184ed97b7b1ef71c984c
SeungyeonWoo <seungyeon3287@gmail.com>
2025-08-11 00:05:34 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/found404/marketbee/MarketBeeApplication.java
@@ -0,0 +1,13 @@
+package com.found404.marketbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketBeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketBeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/found404/marketbee/MarketBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.found404.marketbee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketBeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
