f425237eed1e0f1b2a23935d6ccf5bb6f4b52cf4
SeungyeonWoo <seungyeon3287@gmail.com>
2025-08-22 23:19:30 +0900

    fix(PlaceController): 잘못된 경로 수정

--- src/main/java/com/found404/marketbee/store/place/PlaceController.java
+++ src/main/java/com/found404/marketbee/store/place/PlaceController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/places")
+@RequestMapping("/api/v1/stores")
 @RequiredArgsConstructor
 public class PlaceController {
 
