305e22a8489006d42166fc86f54c8d98814d6a24
SeungyeonWoo <seungyeon3287@gmail.com>
2025-10-28 22:27:38 +0900

    feat: JWT 토큰 발급 및 인증 필터 로직 구현
    JwtProvider를 통해 토큰 생성 및 검증 로직 추가
    JwtAuthenticationFilter에서 요청 헤더 토큰 인증 처리
    CustomUserDetailsService로 사용자 정보 로드 기능 구현

---
+++ src/main/java/com/found404/marketbee/auth/jwt/JwtProvider.java
@@ -0,0 +1,43 @@
+package com.found404.marketbee.auth.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+
+@Component
+public class JwtProvider {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private final long expiration = 1000L * 60 * 60;
+
+    public String createToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
---
+++ src/main/java/com/found404/marketbee/user/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.found404.marketbee.user;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("해당 아이디를 가진 사용자를 찾을 수 없습니다: " + userId));
+    }
+}
